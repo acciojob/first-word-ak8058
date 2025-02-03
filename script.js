aedb337b23f8c1076f054c356c0aafdace14819e
Anil Kumar <114513976+ak8058@users.noreply.github.com>
2025-02-03 09:18:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function firstWord(s) {
   // your code here
+	let strtrim=s.trim();
+	if(strtrim==="")
+	{
+		return '';
+	}
+	const spaceindex=strtrim.indexOf(' ');
+	if(spaceindex===-1)
+	{
+		return strtrim;
+	}
+	return strtrim.slice(0,spaceindex);
 }
 
 // Do not change the code below
+
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
